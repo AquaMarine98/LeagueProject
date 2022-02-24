4a3d610a8e62d3b0c11a383449ce88d05d4170e1
AquaMarine98 <76232991+AquaMarine98@users.noreply.github.com>
2022-02-24 18:04:34 -0300

    Hice todas las boxes para los campeones y arregle que algunos nombres de los campeones no lo tomaba el html

--- JS/createChampionContainer.js
+++ JS/createChampionContainer.js
@@ -1,20 +1,21 @@
 let card = document.getElementById('CHAMPIONS_CONTAINER');
-let div = document.createElement('div');
-let championImg = document.createElement('img');
-let championName = document.createElement('h1');
 
 let champ;
 
 function waitUntilChampNamesIsReady() {
-    for(let i = 0; i < championsName[0].length; i++) {
+    for (let i = 0; i < championsName[0].length; i++) {
+        let div = document.createElement('div');
+        let championImg = document.createElement('img');
+        let championName = document.createElement('h1');
 
         card.appendChild(div);
+        div.setAttribute('id', `${i}`);
         div.classList.add('champion');
         div.appendChild(championImg);
         div.appendChild(championName);
-    
-        let champ = championsName[0][i][1].name;
-    
+
+        champ = championsName[0][i][1].id;
+
         championImg.src = (`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ}_0.jpg`);
         championName.textContent = championsName[0][i][0];
     }

8265289e5a68d5b4e0364227f8635f1c060bd8b2
AquaMarine98 <76232991+AquaMarine98@users.noreply.github.com>
2022-02-25 17:10:39 -0300

    Agregue pequeños cambios

--- JS/createChampionContainer.js
+++ JS/createChampionContainer.js
@@ -4,18 +4,19 @@ let champ;
 
 function waitUntilChampNamesIsReady() {
     for (let i = 0; i < championsName[0].length; i++) {
-        let div = document.createElement('div');
+        let a = document.createElement('a');
         let championImg = document.createElement('img');
         let championName = document.createElement('h1');
 
-        card.appendChild(div);
-        div.setAttribute('id', `${i}`);
-        div.classList.add('champion');
-        div.appendChild(championImg);
-        div.appendChild(championName);
+        card.appendChild(a);
+        a.setAttribute('id', `${i}`);
+        a.classList.add('champion');
+        a.appendChild(championImg);
+        a.appendChild(championName);
 
         champ = championsName[0][i][1].id;
 
+        a.href = "../HTML/campeon.html";
         championImg.src = (`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ}_0.jpg`);
         championName.textContent = championsName[0][i][0];
     }
